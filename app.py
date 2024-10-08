2c8867faf809e4d0bd811ff36a266002f731330d
nicholaslyw <58281828+nicholaslyw@users.noreply.github.com>
2024-10-08 15:29:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Route for the home page
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('main.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['email']
+    # Pass the user input to the result template
+    return render_template('result.html', email=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-@app.route('/')
-def home():
-    user = "John Doe"
-    return render_template('index.html', username=user)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
